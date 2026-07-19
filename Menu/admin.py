5e728a524072ba954dd8fe7305c907a824460b64
Stacy Chege <stacychege23@gmail.com>
2026-07-19 19:06:18 +0300

    Register Product and Category models in admin; add inline support for ProductImage

--- Menu/admin.py
+++ Menu/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage
 
-# Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    # lets you upload product photos directly from the Product edit page,
+    # instead of managing ProductImage as a separate admin section
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'sort_order']
+    prepopulated_fields = {'slug': ('name',)}  # auto-fills slug field in the admin form as you type
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'base_price', 'is_available']
+    list_filter = ['category', 'is_available']
+    search_fields = ['name', 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline]
